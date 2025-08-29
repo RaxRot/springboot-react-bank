75132b5b2eb5727c70e8066e8d6d418d6bd44a77
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-08-29 14:48:56 +0100

    add User service with uploadimg profile,uod username,upd pass,sendrem username

--- back/src/main/java/com/raxrot/back/services/UserService.java
+++ back/src/main/java/com/raxrot/back/services/UserService.java
@@ -1,9 +1,18 @@
 package com.raxrot.back.services;
 
+import com.raxrot.back.dtos.ChangePasswordRequest;
+import com.raxrot.back.dtos.ForgotUsernameRequest;
+import com.raxrot.back.dtos.UpdateUsernameRequest;
+import com.raxrot.back.dtos.UserResponse;
 import com.raxrot.back.security.dto.UserInfoResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.web.multipart.MultipartFile;
 
 public interface UserService {
     ResponseEntity<UserInfoResponse> getUserInfo(Authentication authentication);
+    UserResponse uploadImgProfilePic(MultipartFile file);
+    UserResponse updateUsername(UpdateUsernameRequest request);
+    void updatePassword(ChangePasswordRequest request);
+    void sendUsernameReminder(ForgotUsernameRequest request);
 }
