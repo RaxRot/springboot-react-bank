fb93dcc1ce278cdb863c8abdaa0bc9cb193a2e4c
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-08-29 17:00:00 +0100

    add Account

---
+++ back/src/main/java/com/raxrot/back/models/Account.java
@@ -0,0 +1,46 @@
+package com.raxrot.back.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tbl_accounts")
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private CurrencyType currencyType;
+
+    private BigDecimal balance = BigDecimal.ZERO;
+
+    @Column(nullable = false, unique = true, length = 34)
+    private String iban;
+
+    @Column(nullable = false, length = 11)
+    private String swiftCode = "RAXBANK";
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+}
