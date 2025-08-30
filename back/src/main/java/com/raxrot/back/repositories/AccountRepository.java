9608742d0ff69a36f0f25afb8bfbc0da5d3689df
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-08-30 15:02:19 +0100

    add findByUser_UserId

--- back/src/main/java/com/raxrot/back/repositories/AccountRepository.java
+++ back/src/main/java/com/raxrot/back/repositories/AccountRepository.java
@@ -3,6 +3,9 @@ package com.raxrot.back.repositories;
 import com.raxrot.back.models.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AccountRepository extends JpaRepository<Account, Long> {
     long countByUser_UserId(Long userId);
+    List<Account> findByUser_UserId(Long userId);
 }
